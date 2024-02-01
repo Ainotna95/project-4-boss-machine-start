8bd5126ce6003cf3079ed8e8b6e5f0c6240bf5f1
Ainotna95 <a.mcdonnell123@gmail.com>
2024-02-01 15:34:33 +0000

    adding cors, app.listen and mounting apiRouter

--- project-4-boss-machine-start/server.js
+++ project-4-boss-machine-start/server.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 //import cors middleware
-
+const cors = require('cors');
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -14,17 +14,22 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+//use default settings
+app.use(cors());
 
 // Add middware for parsing request bodies here:
 app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+  });
 }
+
+
